2035c00e8cff6a5ba555e451d0ba5b6e1305d4e3
Fen Dweller <chemicalcrux@gmail.com>
2018-03-18 10:24:18 -0400

    Dark mode evaluates sooner, preventing the page from flickering

---
+++ darkmode.js
@@ -0,0 +1,31 @@
+// do da dark mode
+
+let dark = false;
+
+function toggleDarkMode(e) {
+  dark = !dark;
+  setDarkMode(dark);
+}
+
+function setDarkMode(darkMode) {
+  dark = darkMode;
+  window.localStorage.setItem("dark-mode",dark);
+  if (dark) {
+    document.querySelector("body").classList.remove("light");
+    document.querySelector("body").classList.add("dark");
+  } else {
+    document.querySelector("body").classList.remove("dark");
+    document.querySelector("body").classList.add("light");
+  }
+}
+
+window.addEventListener('load', function(event) {
+
+  (function() {
+    let storage = window.localStorage;
+
+    if (storage.getItem("dark-mode") != null) {
+      setDarkMode(storage.getItem("dark-mode") === "true");
+    }
+  }());
+});
--- game.js
+++ game.js
@@ -15,27 +15,6 @@ window.onerror = function(msg, source, lineno, colno, error) {
   }
 };
 
-// do da dark mode
-
-let dark = false;
-
-function toggleDarkMode(e) {
-  dark = !dark;
-  setDarkMode(dark);
-}
-
-function setDarkMode(darkMode) {
-  dark = darkMode;
-  window.localStorage.setItem("dark-mode",dark);
-  if (dark) {
-    document.querySelector("body").classList.remove("light");
-    document.querySelector("body").classList.add("dark");
-  } else {
-    document.querySelector("body").classList.remove("dark");
-    document.querySelector("body").classList.add("light");
-  }
-}
-
 let started = false;
 
 let strolling = false;
